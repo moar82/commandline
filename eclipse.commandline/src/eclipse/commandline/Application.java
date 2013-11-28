7d69044585c8daa7730612737987fa04c569492d
moar82 <moar82@hotmail.com>
2013-11-28 00:06:10 -0500

    I add the complete namespace of GodClass, because it was necessary to carry the experiment

--- eclipse.commandline/src/eclipse/commandline/Application.java
+++ eclipse.commandline/src/eclipse/commandline/Application.java
@@ -243,7 +243,8 @@ public class Application implements IApplication {
 							List<ExtractClassCandidateRefactoring> candidates = group.getCandidates();
 							for(ExtractClassCandidateRefactoring candidate : candidates) {
 								System.out.println(candidate);
-								writer.write(candidate.getSource().substring(candidate.getSource().lastIndexOf(".")+1)+","+nameVersion);
+								writer.write(candidate.getSource()+","+nameVersion);
+								//writer.write(candidate.getSource().substring(candidate.getSource().lastIndexOf(".")+1)+","+nameVersion);
 								//writer.write(candidate.getSourceEntity().substring(candidate.getSourceEntity().lastIndexOf(".")+1)+","+nameVersion);
 								writer.newLine();
 							}
